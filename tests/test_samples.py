60a6a3cf7661b1f476c9e531fe08d1d56448a3bf
Philipp Spachtholz <philipp.spachtholz@comsysto.com>
2020-12-13 08:15:32 +0100

    Extract base url from schema

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,24 +1,31 @@
-import pdb
-import re
 import pytest
 import subprocess
 import logging
-import shutil
 import yaml
 import tempfile
 import os
 
 from copy import deepcopy
+from dataclasses import dataclass
 from pathlib import Path
 
 log = logging.getLogger(__name__)
 
+@dataclass
 class CodeSample:
-    def __init__(self, endpoint, method, lang, source):
-        self.endpoint = endpoint
-        self.method = method
-        self.lang = lang
-        self.source = source
+    endpoint: str
+    method: str
+    lang: str
+    source: str
+
+def extract_url_from_schema(schema_path):
+    with open(schema_path, 'r') as f:
+        schema = yaml.safe_load(f)    
+    url = schema["servers"][0]["url"]
+    if url[-1] != "/":
+        url += "/"
+    return url
+
     
 def get_code_samples(schema_path):
     """Extract code samples from schema and group by language and method"""
@@ -27,45 +34,38 @@ def get_code_samples(schema_path):
     samples = []
     for path, method_description in schema["paths"].items():
         for method, description in method_description.items():
-            print(description)
-            for example in description.get("x-code-samples", []):
+            examples = description.get("x-code-samples", [])
+            for example in examples:
                 lang = example["lang"]
                 source = example["source"]
                 sample = CodeSample(endpoint=path, method=method, lang=lang, source=source)
                 samples.append(sample)
-    print(samples)
     return samples
 
-
-    
 
 class SampleHandler:
     status_code_msg = None
     subprocess_args = None
 
-    def __init__(self, sample, environment='production'):
+    def __init__(self, base_url, sample, environment='production'):
+        self.base_url = base_url
         self.sample = deepcopy(sample)
         self.environment = environment
 
     def change_api_key(self):
         self.sample.source = self.sample.source.replace('API_TEST_KEY', os.environ["API_TEST_KEY"])
 
-    def change_base_url(self, url='http://localhost:8000/v1'):
-        self.sample.source = self.sample.source.replace(base_url, url)
-
     def add_status_code_message(self):
         self.sample.source = self.sample.source + self.status_code_msg
 
     def run(self):
-        if self.environment == 'development':
-            self.change_base_url()
         self.change_api_key()
         self.add_status_code_message()
         result = self.run_subprocess()
         return result
 
     def run_subprocess(self):
-        handle, path = tempfile.mkstemp(suffix=self.extension)
+        _, path = tempfile.mkstemp(suffix=self.extension)
         with open(path, 'w') as f:
             f.write(self.sample.source)
         args = self.subprocess_args + [path]
@@ -84,16 +84,17 @@ class RHandler(SampleHandler):
     subprocess_args = ['Rscript']
     extension = ".R"
 
+
 class PythonHandler(SampleHandler):
     status_code_msg = '\nimport sys;sys.stderr.write(str(r.status_code))'
     subprocess_args = ['python']
     extension = ".py"
 
+
 class ShellHandler(SampleHandler):
     status_code_msg = ' -s -o /dev/null -w "%{http_code}\\n" 1>&2'
     subprocess_args = ['/bin/sh']
     extension = ".sh"
-
 
 
 def get_language_handler(lang):
@@ -110,20 +111,20 @@ def get_language_handler(lang):
 path = Path('./schema/schema.yaml')
 samples = get_code_samples(schema_path=path)
 
-base_url = 'https://api.features4.com/v1'
+@pytest.fixture
+def base_url():
+    url = os.environ.get("API_BASE_URL") or extract_url_from_schema(schema_path=path)
+    return url
 
-@pytest.mark.parametrize('environment', ['production'])
 @pytest.mark.parametrize('sample', samples)
-def test_code_sample(sample, environment):
+def test_code_sample(base_url, sample):
     handler = get_language_handler(sample.lang)
-    h = handler(sample=sample, environment=environment)
+    h = handler(base_url=base_url, sample=sample)
     s = h.run()
     assert s.stderr.strip() == '200'
 
 
 if __name__ == '__main__':
-    environments = ["production", "development"]
-    for environment in environments:
-        for sample in samples:
-            print(environment, sample.endpoint, sample.lang)
-            test_code_sample(sample, environment)
+    samples = get_code_samples(schema_path=path)
+    for sample in samples:
+        test_code_sample(base_url=base_url, sample=sample)
